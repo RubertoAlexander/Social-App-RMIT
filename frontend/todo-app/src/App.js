4ff9fd2867b71cd1908af6c652a0b6d7e5194409
s3543952 <s3543952@student.rmit.edu.au>
2019-08-10 22:57:00 +1000

    remove placeholder code

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from "react";
-//import FirstComponent from './components/learning-examples/FirstComponent'
-//import SecondComponent from './components/learning-examples/SecondComponent'
-//import ThirdComponent from './components/learning-examples/ThirdComponent'
-//import Counter from './components/counter/Counter'
 import TodoApp from "./components/todo/TodoApp";
 import "./App.css";
 import "./bootstrap.css";
-import ProductComponent from "./components/product/ProductComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<Counter/>*/}
         <TodoApp />
-        <ProductComponent />
       </div>
     );
   }
 }
-
-// class LearningComponents extends Component {
-//   render() {
-//     return (
-//       <div className="LearningComponents">
-//          My Hello World
-//          <FirstComponent></FirstComponent>
-//          <SecondComponent></SecondComponent>
-//          <ThirdComponent></ThirdComponent>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
