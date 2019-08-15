f34c71b4f47a58269634e872a500952edab1db1c
Alexander Ruberto <rubertoalexander@gmail.com>
2019-08-15 23:25:23 +1000

    Map column styling

--- frontend/todo-app/src/components/map/MapComponent.jsx
+++ frontend/todo-app/src/components/map/MapComponent.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper } from 'google-maps-react'
+
+import './Map.css'
 
 class MapComponent extends React.Component {
 	render() {
 		return (
-			<div>
+			<div id="map-component">
 				<h1>This is a map</h1>
-                <Map
-                    google={this.props.google}
-                    zoom={16}
-                    //style={mapStyles}
-                    initialCenter={{lat: -37.8047, lng: 144.9580}}
-                />
+                <div id="map">
+                    <Map
+                        google={this.props.google}
+                        zoom={16}
+                        //style={mapStyles}
+                        initialCenter={{lat: -37.8047, lng: 144.9580}}
+                    />
+                </div>
 			</div>
 		)
 	}
