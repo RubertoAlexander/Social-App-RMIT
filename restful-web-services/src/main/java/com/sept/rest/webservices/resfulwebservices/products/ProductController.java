bcd3b848750fa1904ccd3986916ce03ec95f366d
s3744010 <s3744010@student.rmit.edu.au>
2019-08-17 00:51:20 +1000

    Adding viewing single product feature

--- restful-web-services/src/main/java/com/sept/rest/webservices/resfulwebservices/products/ProductController.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/resfulwebservices/products/ProductController.java
@@ -10,9 +10,14 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
-	@GetMapping(value = { "", "/" })
-    public @NotNull Iterable<Product> getProducts() {
+	@GetMapping(value = "/getAllProducts")
+    public @NotNull Iterable<Product> getAllProducts() {
         return productService.getAllProducts();
     }
+	
+	@GetMapping(value = "/getProduct")
+	public @NotNull Product getProduct(Long id) {
+		return productService.getProduct(id);
+	}
 	
 }
