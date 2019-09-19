e5cf2f8b9d8bceeaa0b0ffef343fdbcc43a93c09
s3543952 <s3543952@student.rmit.edu.au>
2019-09-19 11:32:25 +1000

    merge conflicts

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/register/RegistorJpaResource.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/register/RegistorJpaResource.java
@@ -1,7 +1,8 @@
 package com.sept.rest.webservices.restfulwebservices.register;
 
+import com.sept.rest.webservices.restfulwebservices.jwt.JwtInMemoryUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,46 +12,43 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RestController
 public class RegistorJpaResource {
+    private final UserService userService;
+    private final JwtInMemoryUserDetailsService userDetailsService;
+    private final UserJpaRepository UserJpaRepository;
+    private final PasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
+    public RegistorJpaResource(
+            UserService userService, JwtInMemoryUserDetailsService userDetailsService,
+            UserJpaRepository UserJpaRepository, PasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.userDetailsService = userDetailsService;
+        this.UserJpaRepository = UserJpaRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
-	private UserService userService;
+    @PostMapping("/sign-up")
+    public String signUp(@RequestBody NewUser user) {
+        NewUser exists = UserJpaRepository.findByUsername(user.getUsername());
+        if (exists != null) {
+            return "user already exists";
+        } else {
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            UserJpaRepository.save(user);
+            return "user created";
+        }
+    }
 
-	private JwtInMemoryUserDetailsService userDetailsService;
+    @PostMapping("user/{id}/cashBalance/add/{amount}")
+    public NewUser addCashBalance(@PathVariable double amount, @PathVariable Long id) {
+        NewUser user = null;
+        if (UserJpaRepository.existsById(id)) {
+            user = UserJpaRepository.findById(id)
+                    .get();
+            user.setCashBalance(user.getCashBalance() + amount);
+        }
 
-	private UserJpaRepository UserJpaRepository;
-
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-	@Autowired
-	public RegistorJpaResource(UserService userService, JwtInMemoryUserDetailsService userDetailsService,
-			UserJpaRepository UserJpaRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.userService = userService;
-		this.userDetailsService = userDetailsService;
-		this.UserJpaRepository = UserJpaRepository;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
-
-	@PostMapping("/sign-up")
-	public String signUp(@RequestBody NewUser user) {
-		NewUser exists = UserJpaRepository.findByUsername(user.getUsername());
-		if (exists != null) {
-			return "user already exists";
-		} else {
-			user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-			UserJpaRepository.save(user);
-			return "user created";
-		}
-	}
-
-	@PostMapping("user/{id}/cashBalance/add/{amount}")
-	public NewUser addCashBalance(@PathVariable double amount, @PathVariable Long id) {
-		NewUser user = null;
-		if (UserJpaRepository.existsById(id)) {
-			 user = UserJpaRepository.findById(id).get();
-			 user.setCashBalance(user.getCashBalance() + amount);
-		}
-
-		return user;
-	}
+        return user;
+    }
 
 }
