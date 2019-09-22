dee56c9a2430c7a34baff51d61341192e5878eb2
s3543952 <s3543952@student.rmit.edu.au>
2019-09-22 13:18:29 +1000

    implement new function - list product add display of a single product details

--- frontend/todo-app/src/components/product/ListProduct.jsx
+++ frontend/todo-app/src/components/product/ListProduct.jsx
@@ -3,40 +3,49 @@
  */
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Avatar from "@material-ui/core/Avatar";
-import LockOutlinedIcon from "@material-ui/core/SvgIcon/SvgIcon";
 import Typography from "@material-ui/core/Typography";
-import {Formik} from "formik";
+import { Formik } from "formik";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
 import Container from "@material-ui/core/Container";
+import ProductService from "../../service/ProductService";
 
-class ListProduct extends React.Component {
+export class ListProduct extends React.Component {
   constructor(props) {
     super(props);
   }
+
+  listNewProduct = async values => {
+    try {
+      const result = await ProductService.listNewProduct(
+        values,
+        values.imageUrl
+      );
+      console.log(result);
+    } catch (e) {
+      return e;
+    }
+  };
 
   render() {
     return (
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div>
-          <Avatar>
-            <LockOutlinedIcon />
-          </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            List new product
           </Typography>
           <Formik
             initialValues={{
-              username: "",
-              password: ""
+              productName: "",
+              price: 0,
+              description: "",
+              imageUrl: null
             }}
             validate={values => {}}
             onSubmit={async (values, { setSubmitting }) => {
-              await createAccount(values);
+              await this.listNewProduct(values);
             }}
           >
             {({
@@ -46,7 +55,8 @@ class ListProduct extends React.Component {
               handleChange,
               handleBlur,
               handleSubmit,
-              isSubmitting
+              isSubmitting,
+              setFieldValue
             }) => (
               <form onSubmit={handleSubmit}>
                 <Grid container spacing={2}>
@@ -54,30 +64,65 @@ class ListProduct extends React.Component {
                     <TextField
                       onChange={handleChange}
                       onBlur={handleBlur}
-                      value={values.username}
+                      value={values.name}
                       variant="outlined"
                       required
                       fullWidth
-                      id="username"
-                      label="Username"
-                      name="username"
-                      autoComplete="username"
+                      id="productName"
+                      label="productName"
+                      name="productName"
+                      type="text"
                     />
                   </Grid>
                   <Grid item xs={12}>
                     <TextField
                       onChange={handleChange}
                       onBlur={handleBlur}
-                      value={values.password}
+                      value={values.price}
                       variant="outlined"
                       required
                       fullWidth
-                      name="password"
-                      label="Password"
-                      type="password"
-                      id="password"
-                      autoComplete="new-password"
+                      name="price"
+                      label="price"
+                      id="price"
+                      type="number"
                     />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.description}
+                      variant="outlined"
+                      required
+                      fullWidth
+                      name="description"
+                      label="description"
+                      id="description"
+                      type="text"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <input
+                      accept="image/*"
+                      // className={classes.input}
+                      onBlur={handleBlur}
+                      required
+                      name="imageUrl"
+                      id="imageUrl"
+                      type="file"
+                      onChange={event => {
+                        setFieldValue("imageUrl", event.currentTarget.files[0]);
+                      }}
+                    />
+                    <label htmlFor="imageUrl">
+                      <Button
+                        component="span"
+                        // className={classes.button}
+                      >
+                        Upload Product Image
+                      </Button>
+                    </label>
                   </Grid>
                 </Grid>
                 <Button
@@ -87,15 +132,8 @@ class ListProduct extends React.Component {
                   color="primary"
                   disabled={isSubmitting}
                 >
-                  Sign Up
+                  List new product
                 </Button>
-                <Grid container justify="flex-end">
-                  <Grid item>
-                    <Link href="/login" variant="body2">
-                      Already have an account? Sign in
-                    </Link>
-                  </Grid>
-                </Grid>
               </form>
             )}
           </Formik>
--- frontend/todo-app/src/components/product/ProductDetailComponent.jsx
+++ frontend/todo-app/src/components/product/ProductDetailComponent.jsx
@@ -1,12 +1,81 @@
 import * as React from "react";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Grid from "@material-ui/core/Grid";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 export class ProductDetailComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.productId = props.match.params.id;
   }
 
   render() {
-    return <div>{this.productId}</div>;
+    const { classes } = this.props;
+    return (
+      <Grid container justify="center">
+        <Grid item xs={1}>
+          <IconButton
+            onClick={() => {
+              this.props.setDisplayProductDetail(false);
+            }}
+          >
+            <ArrowBackIcon />
+          </IconButton>
+        </Grid>
+        <Grid
+          className="product"
+          item
+          key={this.props.card.id}
+          xs={11}
+          sm={11}
+          md={11}
+        >
+          <Card className={classes.card}>
+            <CardMedia
+              className={classes.cardMedia}
+              image={this.props.loadImage(this.props.card.id)}
+              title="Image title"
+            />
+            <CardContent className={classes.cardContent}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {this.props.card.name}
+              </Typography>
+              <Typography>{this.props.card.description}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                variant="outlined"
+                onClick={() => {
+                  this.toggleViewProductDetail(this.props.card);
+                }}
+                className="view-button"
+              >
+                View
+              </Button>
+              <Button size="small" color="primary" className="edit-button">
+                Edit
+              </Button>
+
+              <IconButton
+                onClick={() => {
+                  this.props.handleAddToCart(this.props.card);
+                }}
+                color="primary"
+                className={classes.button}
+                aria-label="add to shopping cart"
+              >
+                <AddShoppingCartIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    );
   }
 }
