b9644859ca77558f020e4c68f47deea4595c3c5f
Alexander Ruberto <rubertoalexander@gmail.com>
2019-10-16 11:34:32 +1100

    Added markers to map for classes

--- frontend/todo-app/src/components/map/MapComponent.jsx
+++ frontend/todo-app/src/components/map/MapComponent.jsx
@@ -1,29 +1,203 @@
 import React from "react";
-import { GoogleApiWrapper, Map } from "google-maps-react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 import "./Map.css";
 import Grid from "@material-ui/core/Grid";
+import {
+  Typography,
+  withStyles,
+  Container,
+  Paper,
+  CssBaseline
+} from "@material-ui/core";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+
+import MapService from "../map/MapService";
+import { isNull } from "util";
+
+const styles = theme => ({
+  mapCont: {
+    padding: "1%",
+    height: "90vh"
+  },
+  mapTitle: {
+    paddingTop: "10%",
+    marginLeft: "10%"
+  },
+  map: {
+    height: "89vh",
+    width: "100%",
+    marginLeft: "10%"
+  },
+  toolbar: {
+    padding: "1%"
+  },
+  showClasses: {
+    marginTop: "20%",
+    marginLeft: "10%"
+  }
+});
+
+const tempClasses = [
+  {
+    class_name: "Operating Systems Principles",
+    Description: "Learn all about operating systems",
+    location: "014.06.019"
+  },
+  {
+    class_name: "Professional Computing Practice",
+    Description: "Be professional in the workplace",
+    location: "080.02.007"
+  },
+  {
+    class_name: "Software Engineering: Process and Tools",
+    Description: "The process of software engineering",
+    location: "056.06.088"
+  },
+  {
+    class_name: "Algorithms and Analysis",
+    Description: "Algorithms of Computer Science",
+    location: "014.06.016"
+  }
+];
 
 export class MapComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasClasses: true,
+      showClasses: false
+    };
+    this.retrieveClasses();
+    this.handleShowClasses = this.handleShowClasses.bind(this);
+  }
+
+  handleShowClasses() {
+    this.setState({ showClasses: !this.state.showClasses });
+  }
+
+  retrieveClasses() {
+    MapService.retrieveClasses();
+    /*.then(response => {
+      console.log(response.data);
+      return response.data;
+    }).catch(error => {
+      console.log("ERROR: no classes")
+      this.setState({hasClasses: false})
+    });*/
+  }
+
+  renderClasses = () => {
+    if (this.state.showClasses) {
+      const buildings = this.getClasses();
+      return buildings.map(building => (
+        <Marker
+          key={building.id}
+          position={{
+            lat: this.getBuildingLat(building.building),
+            lng: this.getBuildingLong(building.building)
+          }}
+        />
+      ));
+    }
+  };
+
+  getBuildingLat(building) {
+    if (building == "14") {
+      return -37.8075936;
+    } else if (building == "80") {
+      return -37.808272;
+    } else if (building == "56") {
+      return -37.8051133;
+    }
+  }
+
+  getBuildingLong(building) {
+    if (building == "14") {
+      return 144.9632783;
+    } else if (building == "80") {
+      return 144.9604523;
+    } else if (building == "56") {
+      return 144.963449;
+    }
+  }
+
+  getClasses() {
+    let buildings = [];
+    const classes = JSON.parse(sessionStorage.getItem("classes"));
+    console.log(classes);
+    if (this.state.hasClasses) {
+      for (let i = 0; i < classes.length; i++) {
+        buildings.push({
+          id: i,
+          building: this.getBuildingNumber(classes[i].location)
+        });
+      }
+      return buildings;
+    }
+  }
+
+  getBuildingNumber(classString) {
+    let building = classString.substring(0, 2);
+    return building;
+  }
+
   render() {
+    const { classes } = this.props;
     return (
-      <Grid container id="map-component">
-        <Grid item xs={2}>
-          <h1>RMIT Map</h1>
-        </Grid>
-        <Grid item xs={10} id="map">
-          <Map
-            google={this.props.google}
-            zoom={16}
-            //style={mapStyles}
-            initialCenter={{ lat: -37.808, lng: 144.9645 }}
-          />
-        </Grid>
-      </Grid>
+      <React.Fragment>
+        <CssBaseline />
+        <main id="map">
+          <Container maxWidth="lg">
+            <Paper>
+              <Grid container justify="center">
+                <Grid item xs={2} className={classes.toolbar}>
+                  <Typography
+                    variant="h4"
+                    align="center"
+                    className={classes.mapTitle}
+                  >
+                    RMIT Map
+                  </Typography>
+                  <ToggleButton
+                    className={classes.showClasses}
+                    value="classes"
+                    size="small"
+                    selected={this.state.showClasses}
+                    onChange={this.handleShowClasses}
+                  >
+                    Show Classes
+                  </ToggleButton>
+                </Grid>
+
+                <Grid item xs={8} className={classes.map}>
+                  <LoadScript
+                    id="script-loader"
+                    googleMapsApiKey="AIzaSyALA3BO0lJTnnbZNbHVTd4TwSjSNoqo85Q"
+                  >
+                    <GoogleMap
+                      id="google-map"
+                      mapContainerStyle={{
+                        height: "100%",
+                        width: "100%"
+                      }}
+                      zoom={17}
+                      center={{
+                        lat: -37.808,
+                        lng: 144.964
+                      }}
+                    >
+                      {this.renderClasses()}
+                    </GoogleMap>
+                  </LoadScript>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Container>
+        </main>
+      </React.Fragment>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyALA3BO0lJTnnbZNbHVTd4TwSjSNoqo85Q"
-})(MapComponent);
+export default withStyles(styles)(MapComponent);
---
+++ frontend/todo-app/src/components/map/MapService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { API_URL } from "../../Constants";
+
+class MapService {
+  retrieveClasses() {
+    //let userID = sessionStorage.getItem("user_id");
+    let userID = "2655";
+    return axios
+      .get(`${API_URL}/find/classes/${userID}`)
+      .then(response => {
+        console.log(response.data);
+        sessionStorage.setItem("classes", JSON.stringify(response.data));
+      })
+      .catch(error => {
+        console.log("ERROR: no classes");
+        sessionStorage.setItem("classes", []);
+      });
+  }
+}
+
+export default new MapService();
