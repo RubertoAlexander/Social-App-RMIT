fda526290c5e4f7953b22482d1ae5b51e9ccc010
s3543952 <s3543952@student.rmit.edu.au>
2019-10-17 20:37:41 +1100

    change comment

--- frontend/todo-app/src/components/product/ProductComponent.jsx
+++ frontend/todo-app/src/components/product/ProductComponent.jsx
@@ -8,7 +8,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Sort from "@material-ui/icons/Sort";
 import IconButton from "@material-ui/core/IconButton";
@@ -31,7 +31,7 @@ import product13 from "../../img/product (13).jpg";
 import no_image from "../../img/no-photo-available.png";
 
 import * as lodash from "lodash";
-import { ProductDetailComponent } from "./ProductDetailComponent";
+import {ProductDetailComponent} from "./ProductDetailComponent";
 
 const styles = theme => ({
   icon: {
@@ -54,7 +54,7 @@ const styles = theme => ({
     flexDirection: "column"
   },
   cardMedia: {
-    paddingTop: "56.25%" // 16:9
+    paddingTop: "56.25%" // 16:9 resolution
   },
   cardContent: {
     flexGrow: 1
