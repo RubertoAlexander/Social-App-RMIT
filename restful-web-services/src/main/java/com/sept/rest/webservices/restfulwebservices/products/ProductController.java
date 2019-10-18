ed3939ede11d3340b553262806afd06b0a571bf3
s3543952 <s3543952@student.rmit.edu.au>
2019-10-18 23:26:00 +1100

    resolve conflicts

--- restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/products/ProductController.java
+++ restful-web-services/src/main/java/com/sept/rest/webservices/restfulwebservices/products/ProductController.java
@@ -1,24 +1,16 @@
 package com.sept.rest.webservices.restfulwebservices.products;
 
+import com.sept.rest.webservices.restfulwebservices.dbfile.DBFile;
+import com.sept.rest.webservices.restfulwebservices.dbfile.DBFileService;
 import com.sept.rest.webservices.restfulwebservices.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.sept.rest.webservices.restfulwebservices.dbfile.DBFile;
-import com.sept.rest.webservices.restfulwebservices.dbfile.DBFileService;
-import com.sept.rest.webservices.restfulwebservices.user.UserService;
 
 @CrossOrigin(origins = "*")
 @RestController
